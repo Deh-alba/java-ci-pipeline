45e79cc80f1a5ad18400f82419f0d8803f42c4f5
Andre <andre@Admins-MacBook-Pro-2.local>
2024-10-22 18:32:47 -0300

    just a test

--- src/main/Bootstrapper.java
+++ src/main/Bootstrapper.java
@@ -1,9 +1,10 @@
 package main;
 
+
 public class Bootstrapper {
 
 	public static void main(String[] args) {
-		HelloWorld hw = new HelloWorld("John");
+		HelloWorld hw = new HelloWorld("John john");
 		System.out.println(hw.getHello());
 	}
 }
